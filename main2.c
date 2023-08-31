f1d953ec9e75ddc6fbe8b5a3f607aecbc6436221
12impr <143714796+12impr@users.noreply.github.com>
2023-08-31 17:49:39 +0530

    Add files via upload

---
+++ main2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	char n;
+	printf("enter number");
+	scanf("%c",&n);
+	// int a= len(n);
+	printf("the digit is %d",strlen(n));
+	return 0;
+	for (size_t i = 0; i < count; i++)
+	{
+		/* code */
+	}
+	
+	
+}
